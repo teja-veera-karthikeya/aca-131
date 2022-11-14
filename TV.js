9b5b9b48ae400996a6c4af78b07c8d142c8a027a
teja-veera-karthikeya <105000597+teja-veera-karthikeya@users.noreply.github.com>
2022-11-14 19:58:51 +0530

    Add files via upload

--- TV.js
+++ TV.js
@@ -1,17 +1,42 @@
-
+img="";
+Status="";
+object = [];
+percent="";
 function preload(){
     img=loadImage('tv.png');
 }
 function setup(){
     canvas = createCanvas(640,420);
     canvas.center();
+    objectDetector = ml5.objectDetector('cocossd',modeloaded);
+    document.getElementById("status").innerHTML = "status : Detecting Object";
 
+}
+function modeloaded(){
+    console.log("model Loaded!");
+    Status = true;
+    objectDetector.detect(img,gotResult);
+}
+function gotResult(error,results){
+    if (error){
+        console.log(error);
+    }
+    console.log(results);
+    object=results;
 }
 function draw(){
     image(img,0,0,640,420);
-    fill("#FF0000");
-    text("TV",17,27);
-    noFill();
-    stroke("#FF0000");
-    rect(10,15,517,390)
+    if(Status != ""){
+        for(i =0; i < object.length; i++){
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+
+            fill("#FF0000");
+            percent = floor(object[i].confidence*100);
+            text(object[i].label+""+ percent+"%",object[i].x+15, object[i].y+15);
+            noFill();
+            stroke("#FF0000");
+            rect(object[i].x, object[i].y ,object[i].width ,object[i].height)
+        }
+}
+    
 }
--- bottles.js
+++ bottles.js
@@ -1,17 +1,42 @@
-
+img="";
+Status="";
+object = [];
+percent="";
 function preload(){
-    img=loadImage('bottles.jpg');
+    img=loadImage('bottles.jpeg');
 }
 function setup(){
     canvas = createCanvas(640,420);
     canvas.center();
+    objectDetector = ml5.objectDetector('cocossd',modeloaded);
+    document.getElementById("status").innerHTML = "status : Detecting Object";
 
+}
+function modeloaded(){
+    console.log("model Loaded!");
+    Status = true;
+    objectDetector.detect(img,gotResult);
+}
+function gotResult(error,results){
+    if (error){
+        console.log(error);
+    }
+    console.log(results);
+    object=results;
 }
 function draw(){
     image(img,0,0,640,420);
-    fill("#FF0000");
-    text("BOTTLES",17,27);
-    noFill();
-    stroke("#FF0000");
-    rect(10,15,610,390)
+    if(Status != ""){
+        for(i =0; i < object.length; i++){
+    document.getElementById("status").innerHTML = "status : Detecting Object";
+
+            fill("#FF0000");
+            percent = floor(object[i].confidence*100);
+            text(object[i].label+""+ percent+"%",object[i].x+15, object[i].y+15);
+            noFill();
+            stroke("#FF0000");
+            rect(object[i].x, object[i].y ,object[i].width ,object[i].height)
+        }
+}
+    
 }
